7006b034b2e4fcaf52b54a68806170fd7d52e853
Peafowll <tudorpaun16@gmail.com>
2026-05-30 18:36:06 +0300

    feat : rudimentary blinking arrow

--- FormMeniu.cs
+++ FormMeniu.cs
@@ -8,6 +8,9 @@ namespace proiect_poo
         private Button btnJoaca;
         private Button btnEditor;
         private PictureBox pictureBox1;
+        private Label lblArrowHover;
+        private Timer timerBlinkingArrow;
+        private System.ComponentModel.IContainer components;
         private Button btnIesire;
 
         public FormMeniu()
@@ -45,32 +48,36 @@ namespace proiect_poo
 
         private void InitializeComponent()
         {
+            this.components = new System.ComponentModel.Container();
             this.btnJoaca = new System.Windows.Forms.Button();
             this.btnEditor = new System.Windows.Forms.Button();
             this.btnIesire = new System.Windows.Forms.Button();
             this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.lblArrowHover = new System.Windows.Forms.Label();
+            this.timerBlinkingArrow = new System.Windows.Forms.Timer(this.components);
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.SuspendLayout();
             // 
             // btnJoaca
             // 
-            this.btnJoaca.FlatAppearance.BorderColor = System.Drawing.Color.White;
+            this.btnJoaca.FlatAppearance.BorderColor = System.Drawing.SystemColors.Desktop;
             this.btnJoaca.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.btnJoaca.Font = new System.Drawing.Font("Consolas", 16F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnJoaca.Font = new System.Drawing.Font("Smallest Pixel-7", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.btnJoaca.ForeColor = System.Drawing.SystemColors.Control;
-            this.btnJoaca.Location = new System.Drawing.Point(386, 250);
+            this.btnJoaca.Location = new System.Drawing.Point(386, 252);
             this.btnJoaca.Name = "btnJoaca";
             this.btnJoaca.Size = new System.Drawing.Size(147, 52);
             this.btnJoaca.TabIndex = 0;
             this.btnJoaca.Text = "Play";
             this.btnJoaca.UseVisualStyleBackColor = true;
             this.btnJoaca.Click += new System.EventHandler(this.btnJoaca_Click);
+            this.btnJoaca.MouseEnter += new System.EventHandler(this.FormMeniu_MouseEnter);
             // 
             // btnEditor
             // 
-            this.btnEditor.FlatAppearance.BorderColor = System.Drawing.Color.White;
+            this.btnEditor.FlatAppearance.BorderColor = System.Drawing.SystemColors.Desktop;
             this.btnEditor.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.btnEditor.Font = new System.Drawing.Font("Consolas", 16F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnEditor.Font = new System.Drawing.Font("Smallest Pixel-7", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.btnEditor.ForeColor = System.Drawing.SystemColors.Control;
             this.btnEditor.Location = new System.Drawing.Point(386, 320);
             this.btnEditor.Name = "btnEditor";
@@ -79,12 +86,13 @@ namespace proiect_poo
             this.btnEditor.Text = "Editor";
             this.btnEditor.UseVisualStyleBackColor = true;
             this.btnEditor.Click += new System.EventHandler(this.btnEditor_Click);
+            this.btnEditor.MouseEnter += new System.EventHandler(this.FormMeniu_MouseEnter);
             // 
             // btnIesire
             // 
-            this.btnIesire.FlatAppearance.BorderColor = System.Drawing.Color.White;
+            this.btnIesire.FlatAppearance.BorderColor = System.Drawing.SystemColors.Desktop;
             this.btnIesire.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.btnIesire.Font = new System.Drawing.Font("Consolas", 16F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnIesire.Font = new System.Drawing.Font("Smallest Pixel-7", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.btnIesire.ForeColor = System.Drawing.SystemColors.Control;
             this.btnIesire.Location = new System.Drawing.Point(386, 387);
             this.btnIesire.Name = "btnIesire";
@@ -93,6 +101,7 @@ namespace proiect_poo
             this.btnIesire.Text = "Exit";
             this.btnIesire.UseVisualStyleBackColor = true;
             this.btnIesire.Click += new System.EventHandler(this.btnIesire_Click);
+            this.btnIesire.MouseEnter += new System.EventHandler(this.FormMeniu_MouseEnter);
             // 
             // pictureBox1
             // 
@@ -104,23 +113,70 @@ namespace proiect_poo
             this.pictureBox1.TabIndex = 3;
             this.pictureBox1.TabStop = false;
             // 
+            // lblArrowHover
+            // 
+            this.lblArrowHover.AutoSize = true;
+            this.lblArrowHover.BackColor = System.Drawing.Color.Transparent;
+            this.lblArrowHover.Font = new System.Drawing.Font("Smallest Pixel-7", 20F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblArrowHover.ForeColor = System.Drawing.SystemColors.Control;
+            this.lblArrowHover.Location = new System.Drawing.Point(367, 258);
+            this.lblArrowHover.Name = "lblArrowHover";
+            this.lblArrowHover.Size = new System.Drawing.Size(34, 40);
+            this.lblArrowHover.TabIndex = 4;
+            this.lblArrowHover.Text = ">";
+            this.lblArrowHover.Visible = false;
+            this.lblArrowHover.Click += new System.EventHandler(this.lblArrowHover_Click);
+            // 
+            // timerBlinkingArrow
+            // 
+            this.timerBlinkingArrow.Enabled = true;
+            this.timerBlinkingArrow.Interval = 500;
+            this.timerBlinkingArrow.Tick += new System.EventHandler(this.timerBlinkingArrow_Tick);
+            // 
             // FormMeniu
             // 
             this.BackColor = System.Drawing.SystemColors.Desktop;
             this.ClientSize = new System.Drawing.Size(942, 541);
+            this.Controls.Add(this.lblArrowHover);
             this.Controls.Add(this.pictureBox1);
             this.Controls.Add(this.btnIesire);
             this.Controls.Add(this.btnEditor);
             this.Controls.Add(this.btnJoaca);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
             this.Name = "FormMeniu";
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         private void richTextBox1_TextChanged(object sender, EventArgs e)
         {
 
+        }
+
+        private void timerBlinkingArrow_Tick(object sender, EventArgs e)
+        {
+            lblArrowHover.Visible = !lblArrowHover.Visible;
+        }
+
+        private void lblArrowHover_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void FormMeniu_MouseEnter(object sender, EventArgs e)
+        {
+            Button hoveredButton = (Button)sender;
+
+            lblArrowHover.Left = hoveredButton.Left - lblArrowHover.Width - 10;
+
+            // Align the arrow vertically with the button (the +5 just pushes it down slightly to center it)
+            lblArrowHover.Top = hoveredButton.Top + 5;
+
+            lblArrowHover.Visible = true;
+            timerBlinkingArrow.Start();
         }
     }
 }
