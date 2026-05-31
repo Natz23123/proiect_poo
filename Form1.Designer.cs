c9e2ae585fb22d29ab0225ea62060317d56b248d
Peafowll <tudorpaun16@gmail.com>
2026-05-31 21:33:56 +0300

    feat : pixel font story block

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -36,40 +36,44 @@
             // lblTextHolder
             // 
             this.lblTextHolder.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
-            this.lblTextHolder.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.lblTextHolder.Location = new System.Drawing.Point(9, 237);
+            this.lblTextHolder.Font = new System.Drawing.Font("Pixel Operator", 14F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblTextHolder.Location = new System.Drawing.Point(14, 365);
+            this.lblTextHolder.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.lblTextHolder.Name = "lblTextHolder";
-            this.lblTextHolder.Size = new System.Drawing.Size(818, 173);
+            this.lblTextHolder.Size = new System.Drawing.Size(1226, 265);
             this.lblTextHolder.TabIndex = 0;
             this.lblTextHolder.Click += new System.EventHandler(this.lblTextHolder_Click);
             // 
             // panelButoane
             // 
             this.panelButoane.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
-            this.panelButoane.Location = new System.Drawing.Point(12, 13);
+            this.panelButoane.Location = new System.Drawing.Point(18, 20);
+            this.panelButoane.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.panelButoane.Name = "panelButoane";
-            this.panelButoane.Size = new System.Drawing.Size(512, 202);
+            this.panelButoane.Size = new System.Drawing.Size(767, 310);
             this.panelButoane.TabIndex = 9;
             // 
             // panelHUD
             // 
             this.panelHUD.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
-            this.panelHUD.Location = new System.Drawing.Point(559, 12);
+            this.panelHUD.Location = new System.Drawing.Point(838, 18);
+            this.panelHUD.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.panelHUD.Name = "panelHUD";
-            this.panelHUD.Size = new System.Drawing.Size(268, 204);
+            this.panelHUD.Size = new System.Drawing.Size(401, 313);
             this.panelHUD.TabIndex = 10;
             // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.SystemColors.Desktop;
             this.BackgroundImage = global::proiect_poo.Properties.Resources.scanlines_pixels_2;
-            this.ClientSize = new System.Drawing.Size(839, 450);
+            this.ClientSize = new System.Drawing.Size(1258, 692);
             this.Controls.Add(this.panelHUD);
             this.Controls.Add(this.panelButoane);
             this.Controls.Add(this.lblTextHolder);
             this.ForeColor = System.Drawing.SystemColors.Control;
+            this.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.MaximizeBox = false;
             this.MinimizeBox = false;
             this.Name = "Form1";
